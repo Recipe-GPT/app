1e3ee97e8c2a3d96bcf2cdc58896544810fdf9ba
jeongseungmin <jsm8109jsm@gmail.com>
2023-05-08 19:24:28 +0900

    CHORE :: theme color 추가

--- styles/theme.tsx
+++ styles/theme.tsx
@@ -2,10 +2,20 @@ import { DefaultTheme } from "styled-components";
 
 export const theme: DefaultTheme = {
   color: {
-    mainYellow: "#F0C030",
+    lightYellow: "#F0C030",
     serveYellow: "#FBECC1",
     lightGreen: "#96B066",
     mainBrown: "#4C2321",
     mainGrey: "#41414B",
+    lightGrey: "#535360",
+    serveGreen: "rgba(150, 176, 102, 0.45)",
+    midiumGreen: "#95B161",
+    darkGreen: "#647740",
+    mainYellow: "#FFD760",
+    grey900: "#85858F",
+    grey800: "#AAAAB1",
+    grey700: "#DDDDDF",
+    grey600: "#EDEDED",
+    backgroundYellow: "#FFFCF4",
   },
 };
