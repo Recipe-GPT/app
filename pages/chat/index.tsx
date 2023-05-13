5f6f596faa19436239559421d6eb0e820b89d575
jeongseungmin <jsm8109jsm@gmail.com>
2023-05-13 14:50:26 +0900

    FEAT :: 사이드바 퍼블리싱

--- pages/chat/index.tsx
+++ pages/chat/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import * as S from "./style";
 import Header from "@/components/etc/Header";
+import Sidebar from "@/components/etc/Sidebar";
 
 function ChatPage() {
   return (
     <>
       <Header />
+      <Sidebar />
     </>
   );
 }
