d07f5fbd80a2b3f4d29f4772141677d44ea57efc
jeongseungmin <jsm8109jsm@gmail.com>
2023-05-13 13:03:19 +0900

    FEAT :: 페이지별 헤더 글자, 테두리 색상 변경

---
+++ pages/chat/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import * as S from "./style";
+import Header from "@/components/etc/Header";
+
+function ChatPage() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default ChatPage;
