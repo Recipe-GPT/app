ec9fe91c4558ba644e1a2bb9f70803f72e42ac1f
sookyoungwoo <sookyoung2005@gmail.com>
2023-05-21 23:56:08 +0900

    Chore :: 충돌

---
+++ styles/theme.tsx
@@ -0,0 +1,16 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  color: {
+    lightYellow: "#FFE289",
+    serveYellow: "#FFF7DE",
+    mainGrey: "#41414B",
+    lightGrey: "#535360",
+    mainYellow: "#FFD760",
+    grey900: "#85858F",
+    grey800: "#AAAAB1",
+    grey700: "#DDDDDF",
+    grey600: "#EDEDED",
+    backgroundYellow: "#FFFCF4",
+  },
+};
