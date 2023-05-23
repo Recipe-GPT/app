b4bdc036a0b68645707079abaed510ffb992227f
sookyoungwoo <sookyoung2005@gmail.com>
2023-05-23 14:58:55 +0900

    ADD :: 레시피 부분 퍼블리싱 완료

--- components/Detail/DetailRecipe/index.tsx
+++ components/Detail/DetailRecipe/index.tsx
@@ -4,6 +4,19 @@ import * as S from "./style";
 function DetailRecipe() {
   return (
     <>
+      <S.DetailRecipe>
+        <S.Title>레시피</S.Title>
+        <S.MainHr />
+        <S.Recipe>
+          <S.RecipeStep>
+            <S.RecipeTop>
+              <S.Step>step 1</S.Step>
+              <S.StepStory>먹기좋게~</S.StepStory>
+            </S.RecipeTop>
+            <S.RecipeHr/>
+          </S.RecipeStep>
+        </S.Recipe>
+      </S.DetailRecipe>
     </>
   );
 }
--- components/Detail/DetailRecipe/style.ts
+++ components/Detail/DetailRecipe/style.ts
@@ -1,1 +1,49 @@
 import styled from "styled-components";
+
+export const DetailRecipe = styled.div`
+  width: 50vw;
+  margin: 0 auto;
+  padding-top: 2rem;
+  padding-bottom: 10rem;
+`;
+
+export const Title = styled.p`
+  font-size: 1.4rem;
+  padding-bottom: 0.8rem;
+  font-family: "Pretendard-Light";
+`;
+
+export const MainHr = styled.hr`
+  background-color: ${({ theme }) => theme.color.mainYellow};
+  height: 2px;
+  border: 0;
+`;
+
+export const Recipe = styled.div`
+`;
+
+export const RecipeStep = styled.div`
+`;
+
+export const RecipeTop = styled.div`
+  display: flex;
+  padding: 1.2rem 0rem;
+  align-items: center;
+`;
+
+export const Step = styled.p`
+  font-size: 1.1rem;
+  font-family: "Pretendard-Light";
+`;
+
+export const StepStory = styled.p`
+  padding-left: 1.5rem;
+  font-family: "Pretendard-Light";
+  font-size: 0.8rem;
+`;
+
+export const RecipeHr = styled.hr`
+  background-color: ${({ theme }) => theme.color.grey700};
+  height: 1px;
+  border: 0;
+`;
