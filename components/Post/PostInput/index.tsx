d8d4e73f51eda90bf33ee2b6c442eca751f0eece
jeongseungmin <jsm8109jsm@gmail.com>
2023-05-23 16:06:33 +0900

    FEAT :: PostInput

---
+++ components/Post/PostInput/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import * as S from "./style";
+
+function PostInput({ ...props }) {
+  return <S.PostInput {...props} />;
+}
+
+export default PostInput;
---
+++ components/Post/PostInput/style.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const PostInput = styled.input<{ isBig?: true; width?: number }>`
+  border: none;
+  border-bottom: 1px solid black;
+  transition: 0.1s;
+  font-size: ${({ isBig }) => (isBig ? "1.75rem" : "1.25rem")};
+  line-height: 200%;
+  width: ${({ width }) => (width ? `${width}px` : "100%")};
+  &::placeholder {
+    color: #d1d1d7;
+  }
+  &:focus-visible {
+    border-bottom: 1px solid ${({ theme }) => theme.color.mainYellow};
+    outline: none;
+  }
+`;
