245f16cbb91436117708389cd029985ff99d77c6
jeongseungmin <jsm8109jsm@gmail.com>
2023-05-31 10:17:34 +0900

    SET :: react-query, axios 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,18 @@ import { ThemeProvider } from "styled-components";
 import { theme } from "@/styles/theme";
 import GlobalStyle from "@/styles/globals";
 import { RecoilRoot } from "recoil";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
   return (
     <RecoilRoot>
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-        <GlobalStyle />
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+          <GlobalStyle />
+        </ThemeProvider>
+      </QueryClientProvider>
     </RecoilRoot>
   );
 }
