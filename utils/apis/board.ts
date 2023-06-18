a989e1ebdb4e47015559fcf4856ec654504470e6
jeongseungmin <jsm8109jsm@gmail.com>
2023-06-19 02:49:23 +0900

    FIX :: 글쓰기 성공 시 커뮤니티 페이지로 이동

--- utils/apis/board.ts
+++ utils/apis/board.ts
@@ -1,11 +1,15 @@
 import { getAccessToken } from "@/functions/getAccessToken";
 import { instance } from "../instance";
 import { useMutation } from "react-query";
+import { useRouter } from "next/router";
 
 export const postBoard = async (data: FormData) => {
   return (await instance.post("board", data, getAccessToken(true))).data;
 };
 
 export const postBoardMutation = (data: FormData) => {
-  return useMutation(() => postBoard(data));
+  const router = useRouter();
+  return useMutation(() => postBoard(data), {
+    onSuccess: () => router.replace("/board"),
+  });
 };
