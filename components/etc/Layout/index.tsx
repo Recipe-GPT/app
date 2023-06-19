0af279d4ff6a6e1aa11492f4afc58bf7d05f3973
jeongseungmin <jsm8109jsm@gmail.com>
2023-06-19 22:04:17 +0900

    FEAT :: 레시피 추천 질문 실패 시 에러 처리

--- components/etc/Layout/index.tsx
+++ components/etc/Layout/index.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import { useRecoilState } from "recoil";
 import Header from "../Header";
 import Loading from "../Loading";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function Layout() {
   const [isLoading, setIsLoading] = useRecoilState(isLoadingState);
@@ -10,6 +12,18 @@ function Layout() {
   return (
     <>
       <Header />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
       {isLoading && <Loading />}
     </>
   );
