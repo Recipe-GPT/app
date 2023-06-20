6b2e40a7175d9749bb212f9edb16f4b476522eb6
jeongseungmin <jsm8109jsm@gmail.com>
2023-06-20 09:13:00 +0900

    FEAT :: 에러 바운더리 토스트 모든 query/mutation에 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,15 +6,38 @@ import { RecoilRoot } from "recoil";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import Layout from "@/components/etc/Layout";
+import { toast } from "react-toastify";
+import { AxiosError } from "axios";
+import { getRefreshTokenMutation } from "@/utils/apis/auth";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const errorBoundary = (error: unknown) => {
+    const axiosError = error as AxiosError<{
+      message: string;
+      field?: { [key: string]: string };
+    }>;
+    if (!!axiosError.response?.data.field) {
+      Object.values(axiosError.response?.data.field).forEach((message) =>
+        toast.error(message),
+      );
+    } else {
+      toast.error(axiosError?.response?.data?.message);
+    }
+  };
+
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
         retry: false,
+        onError: (error: unknown) => {
+          errorBoundary(error);
+        },
       },
       mutations: {
         retry: false,
+        onError: (error: unknown) => {
+          errorBoundary(error);
+        },
       },
     },
   });
