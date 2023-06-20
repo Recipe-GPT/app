c10579b80e327b0541768d2cdccdd052d7b08ea8
jeongseungmin <jsm8109jsm@gmail.com>
2023-06-20 09:13:09 +0900

    FEAT :: RefreshToken 가져오기

--- components/etc/HeaderLogin/index.tsx
+++ components/etc/HeaderLogin/index.tsx
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from "react";
 import * as S from "./style";
-import { getMyInfoQuery } from "@/utils/apis/user";
 import { useRouter } from "next/router";
-import { getLogoutMutation } from "@/utils/apis/auth";
+import { getLogoutMutation, getRefreshTokenMutation } from "@/utils/apis/auth";
+import { useMutation, useQuery } from "react-query";
+import { getMyInfo, getMyInfoQuery } from "@/utils/apis/user";
+import { instance } from "@/utils/instance";
+import { AxiosError, InternalAxiosRequestConfig } from "axios";
 
 function HeaderLogin() {
   const { pathname } = useRouter();
   const [mount, setMount] = useState(false);
+
+  const getRefreshTokenFunc = getRefreshTokenMutation();
 
   useEffect(() => {
     setMount(true);
@@ -14,6 +19,44 @@ function HeaderLogin() {
   }, []);
 
   const myInfoQuery = getMyInfoQuery(mount);
+  const getRefreshToken = async (): Promise<string | void> => {
+    try {
+      getRefreshTokenFunc.mutate();
+      console.log(getRefreshTokenFunc);
+      if (getRefreshTokenFunc.data?.accessToken !== undefined) {
+        localStorage.setItem(
+          "accessToken",
+          getRefreshTokenFunc.data?.accessToken,
+        );
+      }
+      return getRefreshTokenFunc.data?.accessToken;
+    } catch (e) {
+      localStorage.removeItem("accessToken");
+      localStorage.removeItem("refreshToken");
+    }
+  };
+  instance.interceptors.response.use(
+    (res) => res,
+    (err: AxiosError): Promise<AxiosError> => {
+      if (!localStorage.refreshToken) {
+        return Promise.reject(err);
+      }
+      const { config, response } = err;
+      const originalRequest = config as InternalAxiosRequestConfig;
+      if (response?.status !== 401) {
+        return Promise.reject(err);
+      }
+
+      const accessToken = getRefreshToken();
+
+      // if (accessToken) {
+      //   originalRequest.headers.token = accessToken;
+      //   return instance(originalRequest);
+      // }
+
+      return Promise.reject(err);
+    },
+  );
 
   const logoutMutation = getLogoutMutation();
 
