71ea2d950478db693a3b52c1939b7c14df50c7ea
jeongseungmin <jsm8109jsm@gmail.com>
2023-06-27 20:26:55 +0900

    CHORE :: Trivial

--- components/Board/BoardMainSection/index.tsx
+++ components/Board/BoardMainSection/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import * as S from "./style";
 import BoardRecipeButton from "@/components/etc/Button/BoardRecipeButton";
 import BoardItem from "../BoardItem";
--- components/etc/Loading/style.ts
+++ components/etc/Loading/style.ts
@@ -8,7 +8,7 @@ export const Loading = styled.div`
   position: fixed;
   top: 0;
   left: 0;
-  z-index: 100;
+  z-index: 1000;
   display: flex;
   align-items: center;
   justify-content: center;
